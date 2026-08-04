b6b58589062429e9e8db870f9415bdab855c3a95
yasindural <yasdural@gmail.com>
2026-08-04 22:08:08 +0300

    feat: match accepted spectral interface

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,2 @@
-import React from 'react';import ReactDOM from 'react-dom/client';import {BrowserRouter} from 'react-router-dom';import {App} from './App';import './styles.css';if(!localStorage.getItem('dilmac-palette-v2')){localStorage.setItem('dilmac-theme','light');localStorage.setItem('dilmac-palette-v2','1')}
+import React from 'react';import ReactDOM from 'react-dom/client';import {BrowserRouter} from 'react-router-dom';import {App} from './App';import './styles.css';if(!localStorage.getItem('dilmac-spectral-v3')){localStorage.setItem('dilmac-theme','dark');localStorage.setItem('dilmac-spectral-v3','1')}
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter basename="/dilmac"><App/></BrowserRouter></React.StrictMode>);
